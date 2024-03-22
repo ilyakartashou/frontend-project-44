cf7b05e580e2f0a105978a90072f030a7ce989d4
Ilya Kartashou <you@example.com>
2024-03-22 16:55:13 +0300

    modifing

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-/*eslint-disable no-console, import/extensions */
+/*eslint-disable no-console, import/extensions*/
 import greeting from '../src/cli.js';
-greeting()
+greeting();

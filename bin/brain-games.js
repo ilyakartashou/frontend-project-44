b650248545199c854dec3034fe50557f29c1f74a
Ilya Kartashou <you@example.com>
2024-03-23 12:18:48 +0300

    addind some features

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
-/*eslint-disable no-console, import/extensions*/
+/* eslint-disable no-console, import/extensions */
 import greeting from '../src/cli.js';
+import { questionToUser } from '../src/cli.js';
+
 greeting();
+questionToUser();
--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,11 @@
+/* eslint-disable no-console, import/extensions */
+import readlineSync from 'readline-sync';
+
+export const questionToUser = () => {
+  const username = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${username}`);
+};
+
 export default () => {
-    console.log('Welcome to the Brain Games!');
+  console.log('Welcome to the Brain Games!');
 };

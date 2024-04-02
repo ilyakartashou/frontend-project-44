32b22d331f3ad6973de8f5130731452675414e20
Ilya Kartashou <you@example.com>
2024-04-03 00:54:28 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ export default (instructionsToUser, generateLogik) => {
     if (correctAnswer === userAnswer) {
       console.log('Correct!');
     } else {
-      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.\nLet's try again, ${userName}`);
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.\nLet's try again, ${userName}!`);
       return;
     }
   }

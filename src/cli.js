8c26785957d2e2d331462ae7c3cb1b8b0e43aecc
Ilya Kartashou <you@example.com>
2024-04-03 00:33:05 +0300

    refactoring

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-console, import/extensions, no-param-reassign */
-
+import readlineSync from 'readline-sync';
 export default () => {
   console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${userName}`);
 };
